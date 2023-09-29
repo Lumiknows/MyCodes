fbc9ae9a976b231b092cacbceaac6c17a2cdfe68
randomNumbers <73021617+Lumiknows@users.noreply.github.com>
2023-09-29 21:31:04 +0800

    Update2 MyArray.java

--- MyArray.java
+++ MyArray.java
@@ -12,32 +12,57 @@ public class MyArray {
         ctr = 0;
     }
 
-    boolean isFull() { return ctr >= size;}
-    boolean isEmpty() { return ctr == 0;}
+    boolean isFull() { 
+        return ctr >= size;
+    }
+    boolean isEmpty() { 
+        return ctr == 0;
+    }
 
-    public void add() {
-        while(!isFull()) {
-            System.out.println("Enter Value to Add: ");
-            int val = sc.nextInt();
-            if (isFull()) {
-                System.out.println("Array is Full!");
-            }
-            arr[++idx] = val;
-            ctr++;
-        }
+public void add() {
+    while (true) {
         if (isFull()) {
             System.out.println("Array is Full!");
+            System.out.println("Do you Want to Resize your Array?");
+            System.out.println("1 - Yes     2 - No");
+            int option = sc.nextInt();
+
+            if (option == 1) {
+                arraySize();
+            } else if (option == 2) {
+                return;
+            } else {
+                System.out.println("Invalid Input!");
+            }
+        } else {
+            System.out.println("Enter Value to Add: ");
+            int val = sc.nextInt();
+
+            arr[++idx] = val;
+            ctr++;
+            
+            System.out.println("Do you want to add another Value?");
+            System.out.println("1 - Yes     2 - No");
+            int opt = sc.nextInt();
+
+            if (opt != 1) {
+                return;
+            }
         }
     }
+}
 
     public void view () {
-        for(int i = 0; i < ctr; i++) {
-            System.out.println("[ " + arr[i] + " ]");
-        }
-    }
 
-    public void count() {
-        System.out.println("Current Array Size " + ctr);
+        if (isEmpty()) {
+            System.out.println("Array is Empty!");
+        }
+        
+        for(int i = 0; i < ctr; i++) {
+
+            System.out.print("[" + arr[i] + "] ");
+        }
+            System.out.println("\n\nCurrent Array Size: [" + ctr + "]");
     }
 
     public void remove () {
@@ -51,14 +76,14 @@ public class MyArray {
             if (remIdx < 0 || remIdx >= ctr) {
                 System.out.println("Invalid Index Number!");
             } else {
-                for (int i = remIdx; i < ctr - 1; i ++) {
-                    arr[i] = arr[i +1];
+                for (int i = remIdx; i < ctr - 1; i++) {
+                    arr[i] = arr[i + 1];
                 }
                 ctr--;
-                System.out.println("Index " + remIdx + " was removed!");
+                idx--;
+                System.out.println("Index: " + remIdx + " was removed!");
             }
         }
-
     }
 
     public void search () {
@@ -66,18 +91,27 @@ public class MyArray {
         if (isEmpty()) {
             System.out.println("Array is Empty!");
         } else {
-            System.out.println("Enter the Index to Search: ");
+            System.out.println("Enter the Value to Search: ");
             int scanIdx = sc.nextInt();
 
+            int [] idxList = new int[ctr];
+            int ctrList = 0;
             boolean found = false;
 
             for (int i = 0; i < ctr; i++) {
                 if (arr[i] == scanIdx) {
-                    System.out.println("Value: " + scanIdx + " Index: " + i);
+                    idxList[ctrList++] = i;
                     found = true;
-                    break;
                 }
             }
+            if (found) {
+                System.out.print("Value: " + scanIdx + " found at Index/es: ");
+                for (int x = 0; x < ctrList; x++) {
+                    System.out.print("[" + idxList[x] + "] ");
+                }
+                System.out.println(" ");
+            }
+
 
             if(!found) {
                 System.out.println("Value: " + scanIdx + " was not found!");
@@ -90,21 +124,38 @@ public class MyArray {
         if (isEmpty()) {
             System.out.println("Array is Empty!");
         } else {
-            for (int i = 0; i < ctr - 1; i++) {
-                for (int a = 0; a < ctr - i - 1; a++) {
-                    if (arr[a] > arr [a + 1]) {
-                        int temp = arr[a];
-                        arr[a] = arr[a +1];
-                        arr[a + 1] = temp;
+
+            System.out.println("Choose an Option: ");
+            System.out.println("1 - Ascending   2 - Descending");
+            int option = sc.nextInt();
+
+            if (option == 1) {
+                for (int i = 0; i < ctr - 1; i++) {
+                    for (int a = 0; a < ctr - i - 1; a++) {
+                        if (arr[a] > arr [a + 1]) {
+                            int temp = arr[a];
+                            arr[a] = arr[a + 1];
+                            arr[a + 1] = temp;
+                        }
                     }
                 }
+                System.out.println("Array was sorted in [Ascending] Order successfully!");
+            } else if (option == 2) {
+                for (int i = 0; i < ctr - 1; i++) {
+                    for (int a = 0; a < ctr - i - 1; a++) {
+                        if (arr[a] < arr[a + 1]) {
+                            int temp = arr[a];
+                            arr[a] = arr[a + 1];
+                            arr[a + 1] = temp;
+                        }
+                    }
+                }  
+                System.out.println("Array was sorted in [Descending] Order successfully!");
+            } else {
+                System.out.println("Invalid Input!");
             }
-
-            System.out.println("Array was sorted successfully!");
         }
-
     }
-
     public void edit () {
 
         if (isEmpty()) {
@@ -116,7 +167,7 @@ public class MyArray {
             if (editIdx < 0 || editIdx >= ctr) {
                 System.out.println("Invalid Index!");
             } else {
-                System.out.println("Enter new Value for:  [" + editIdx + "]");
+                System.out.println("Enter new Value for Index:  [" + editIdx + "]");
                 int newVal = sc.nextInt();
                 
                 arr[editIdx] = newVal;
@@ -156,11 +207,11 @@ public class MyArray {
 
         while (true) {
 
-            System.out.println("1 - Add     2 - View");
-            System.out.println("3 - Remove  4 - Search");
-            System.out.println("5 - Sort    6 - Edit");
-            System.out.println("7 - Count   8 - Resize");
-            System.out.println("9 - Exit");
+            System.out.println(" ");
+            System.out.println("[1] - Add     [2] - View");
+            System.out.println("[3] - Remove  [4] - Search");
+            System.out.println("[5] - Sort    [6] - Edit");
+            System.out.println("[7] - Exit   ");
             int choice = sc.nextInt();
 
             switch (choice) {
@@ -183,12 +234,6 @@ public class MyArray {
                 a.edit();
                 break;
             case 7:
-                a.count();
-                break;
-            case 8:
-                a.arraySize();
-                break;
-            case 9:
                 a.exit();
                 break;
             }
