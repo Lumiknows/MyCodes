b7a1bedc0a341c774b605a9643f0bf971760972a
randomNumbers <73021617+Lumiknows@users.noreply.github.com>
2023-10-10 11:43:27 +0700

    Update DistanceCalculator.java

--- DistanceCalculator.java
+++ DistanceCalculator.java
@@ -3,61 +3,67 @@ import java.util.Scanner;
 public class DistanceCalculator {
     public static void main (String [] args) {
         Menu a = new Menu();
-        a.displayMenu();
+        
         a.input();
+        a.displayMenu();
         a.result();
     }
 }
 
 class Menu {
     Scanner sc = new Scanner(System.in);
-    double distance;
-    double est;
+    double speed, distance, est, totaDis;
 
     public void input() {
         System.out.println("Enter Speed(km/hr): ");
-        int speed = sc.nextInt();
-
-        est = distance / speed;
+        speed = sc.nextInt();
     }
-
-    public void result() {
-        System.out.println("EST. Time of Arrival: " + est);
-    }
-    
     public void displayMenu() {
+        
         System.out.println("Enter your Destination: ");
         
         System.out.println("Route 1 - Mingla");
         System.out.println("Route 2 - San Fernando");
         System.out.println("Route 3 - Carcar");
         
+        System.out.println("Route 4.1 - Barili");
         System.out.println("Is Barili Blocked?");
         System.out.println("[1] - Yes [2] - No");
         int option = sc.nextInt();
         
         if (option == 1) {
-            System.out.println("4.2 - Sibonga");
+            System.out.println("Route 4.2 - Sibonga");
             System.out.println("Is Dumanjug Blocked?");
             System.out.println("[1] - Yes [2] - No");
             int route4 = sc.nextInt();
             if (route4 == 1 ) {
-                distance = 92.8;
+                distance = 95.4;
+                totalDis = 95.4;
                 
-                System.out.println("Ronda");
-                System.out.println("Alcantara");
+                System.out.println("Route 5 Argao");
+                System.out.println("Route 5.1 Ronda");
+                System.out.println("Route 5.2 Alcantara");
                 System.out.println("Moalboal (End)");
                 
             } else if (route4 == 2) {
-                distance = 89.7;
-                System.out.println("4.1.2 - Alcantara");
+                distance = 95.1;
+                System.out.println("Route 4.2 - Sibonga");
+                System.out.println("Route 4.2.1 - Dumanjug");
+                System.out.println("Route 4.1.2 - Alcantara");
                 System.out.println("Moalboal (End)");
             }    
-    } else if (option == 2) {
-        distance = 84.4;
-        System.out.println("4.1 Barili");
-        System.out.println("4.1.1 Dumanjug");
-        System.out.println("4.1.2 Alcantara");
-        System.out.println("Moalboal (End)");
+        } else if (option == 2) {
+            distance = 86.1;
+            System.out.println("Route 4.1 Barili");
+            System.out.println("Route 4.1.1 Dumanjug");
+            System.out.println("Route 4.1.2 Alcantara");
+            System.out.println("Moalboal (End)");
+        }
+    }
+    
+    public void result() {
+        est = distance / speed;
+        System.out.printf("\nEST. Time of Arrival: %.2f" ,est);
+        System.out.println("Total Distance: " + totalDis);
     }
 }
